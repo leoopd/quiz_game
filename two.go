847c40472991884b09cffa1e0322ba1b4edf70f6
leo <leo.opdenplatz@gmail.com>
2022-09-06 19:50:14 +0200

    tryig to implement a timer logic that cancels the quiz after a certain amount of time

--- two.go
+++ two.go
@@ -6,9 +6,10 @@ import (
 	"io"
 	"log"
 	"os"
+	"time"
 )
 
-func openAndParseCsv(location string) []string {
+func openAndParseCsv2(location string) []string {
 
 	//Opens the .csv file that's specified as the argument
 	//and returns the content as an array of strings.
@@ -35,7 +36,12 @@ func openAndParseCsv(location string) []string {
 	return quiz
 }
 
-func countPoints(quiz []string) {
+func timer(c chan *time.Time) {
+	timer := time.NewTimer(100 * time.Second)
+	<-timer.C
+}
+
+func countPoints2(quiz []string, pnts chan int, qnts chan int) {
 
 	//Compares user input via the console to the answer and counts
 	//how many questions were asked, how many were answered correctly.
@@ -51,9 +57,38 @@ func countPoints(quiz []string) {
 
 		if answer == string(quiz[i+1]) {
 			points += 1
+			pnts <- points
 		}
 		questions += 1
 
+		qnts <- questions
 	}
-	fmt.Printf("You scored %d points.\nThere were %d questions in total.\n", points, questions)
+}
+
+func main() {
+
+	pnts := make(chan int, 13)
+	qnts := make(chan int, 13)
+	tmr := make(chan *time.Time)
+
+	go func() {
+		countPoints2(openAndParseCsv2("default.csv"), pnts, qnts)
+	}()
+
+	go func() {
+		timer(tmr)
+	}()
+
+	select {
+	case <-pnts:
+		points := <-pnts
+		questions := <-qnts
+		fmt.Printf("You scored %d points.\nThere were %d questions in total.\n", points, questions)
+
+	case <-tmr:
+		points := <-pnts
+		questions := <-qnts
+		fmt.Printf("You scored %d points.\nThere were %d questions in total.\n", points, questions)
+	}
+
 }
