c8cccb2c602f1ce1b31e2e1beed9ec3e42288e6b
leo <leo.opdenplatz@gmail.com>
2022-09-08 13:17:54 +0200

    trying to make the automatic timeout work

--- eval.go
+++ eval.go
@@ -1,15 +1,15 @@
 package main
 
-// Gets the solutions sent from parse.go via channel s and the answers from ask.go sent via channel answers.
-// Compares the answers to the solutions and returns the amount of points the user scored.
-func Eval(solutions, answers []string) (points int) {
+// // Gets the solutions sent from parse.go via channel s and the answers from ask.go sent via channel answers.
+// // Compares the answers to the solutions and returns the amount of points the user scored.
+// func Eval(solutions, answers []string) (points int) {
 
 
-	for i := 0; i < len(answers); i++ {
-		if answers[i] == solutions[i] {
-			points += 1
-		}
-	}
+// 	for i := 0; i < len(answers); i++ {
+// 		if answers[i] == solutions[i] {
+// 			points += 1
+// 		}
+// 	}
 
-	return points
-}
+// 	return points
+// }
--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 
 // Gets a location for a csv file that contains the questions and answers in the format: question,solutions
 // and 2 channels from main, q to send the questions to ask.go and s to send the solutions to eval.go.
-func ParseCsv(location string, s chan []string) (questions []string) {
+func ParseCsv(location string) (questions, solutions []string) {
 
 	//Opens the .csv file that's specified as the argument and sends
 	//the content as arrays of strings, split into questions and answers.
@@ -24,7 +24,6 @@ func ParseCsv(location string, s chan []string) (questions []string) {
 	defer quizFile.Close()
 
 	r := csv.NewReader(quizFile)
-	var solutions []string
 
 	// var questions []string
 	// var solutions []string
@@ -42,63 +41,60 @@ func ParseCsv(location string, s chan []string) (questions []string) {
 		solutions = append(solutions, row[1])
 
 	}
-	s <- solutions
-	return questions
+	return questions, solutions
 }
 
 // Gets the questions via the channel q from parse.go and 2 channels from main, answers to send the answers given by the user to eval.go
 // and questionCount to send the amount of questions asked to main.go.
-func Asker(questions []string, q chan int) (answersGiven []string) {
+func Asker(questions []string) (answersGiven []string, questionsAsked int) {
 
 	ticker := time.NewTicker(time.Second * 5)
 	var ch chan string
-	var questionsAsked int
 
-	for i := 0; i < len(questions); i++ {
+	// for i := 0; i < len(questions); i++ {
 
-		fmt.Println("What is ", questions[i])
-		questionsAsked += 1
+	go func(ch chan string) {
+		reader := bufio.NewReader(os.Stdin)
+		for i := 0; i < len(questions); i++ {
 
-		go func(ch chan string, i int) {
-			fmt.Println("123")
-			reader := bufio.NewReader(os.Stdin)
-			for {
-				s, err := reader.ReadString('\n')
-				if err != nil {
-					log.Fatal(err)
-					close(ch)
-					return
-				}
-				fmt.Println(s)
-				ch <- s
+			fmt.Println("What is ", questions[i])
+			questionsAsked += 1
+
+			s, err := reader.ReadString('\n')
+			if err != nil {
+				log.Fatal(err)
+				close(ch)
+				return
 			}
-		}(ch, i)
+			ch <- s
+		}
+	}(ch)
 
-	stdinloop:
-		for {
-			select {
-			case answer, ok := <-ch:
-				if !ok {
-					break stdinloop
-				} else {
-					answersGiven = append(answersGiven, strings.Trim(answer, "\n"))
-					fmt.Println(answersGiven)
-				}
-			case <-ticker.C:
-				q <- questionsAsked
-				return answersGiven
+	for {
+		select {
+
+		case answer, ok := <-ch:
+			fmt.Println(ok)
+			if !ok {
+				return answersGiven, questionsAsked
+			} else {
+				fmt.Println("hier2")
+
+				answersGiven = append(answersGiven, strings.Trim(answer, "\n"))
 			}
+		case <-ticker.C:
+			return answersGiven, questionsAsked
 		}
 	}
-	q <- questionsAsked
-	return answersGiven
+	fmt.Println("hier")
+	return answersGiven, questionsAsked
 }
+
+// }
 
 // Gets the solutions sent from parse.go via channel s and the answers from ask.go sent via channel answers.
 // Compares the answers to the solutions and returns the amount of points the user scored.
-func Eval(answersGiven []string, s chan []string) (points int) {
-
-	solutions := <-s
+func Eval(answersGiven, solutions []string) (points int) {
 
 	for i := 0; i < len(answersGiven); i++ {
 		if answersGiven[i] == solutions[i] {
@@ -111,24 +107,13 @@ func Eval(answersGiven []string, s chan []string) (points int) {
 
 func main() {
 
-	s := make(chan []string)
-	q := make(chan int)
+	//Seems to work
+	questions, solutions := ParseCsv("default.csv")
 
-	// answers := make(chan []string)
-	// questionCount := make(chan int)
+	//
+	answers, questionsAsked := Asker(questions)
+	fmt.Println(answers, questionsAsked)
 
-	// ParseCsv("default.csv", q, s)
-	// Asker(q, answers, questionCount)
-
-	// points := Eval(s, answers)
-	// questionsAsked := <-questionCount
-
-	// fmt.Printf("Points scored: %d\nQuestions asked: %d", points, questionsAsked)
-
-	fmt.Println(Asker(ParseCsv("default.csv", s), q))
-
-	// questions := <-q
-
-	// fmt.Printf("questions: %v, solutions: %v\n", questions, points)
-
+	points := Eval(answers, solutions)
+	fmt.Println(points)
 }
