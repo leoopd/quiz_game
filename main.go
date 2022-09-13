e8e7718a8f21a7d28acae5d322c9c04a7a7b0716
leo <leo.opdenplatz@gmail.com>
2022-09-13 13:58:52 +0200

    changed some code in main

--- main.go
+++ main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"bufio"
+	"context"
 	"encoding/csv"
 	"fmt"
 	"io"
 	"log"
 	"os"
-	"strings"
 	"time"
 )
 
@@ -25,9 +24,6 @@ func ParseCsv(location string) (questions, solutions []string) {
 
 	r := csv.NewReader(quizFile)
 
-	// var questions []string
-	// var solutions []string
-
 	for {
 		row, err := r.Read()
 
@@ -44,53 +40,43 @@ func ParseCsv(location string) (questions, solutions []string) {
 	return questions, solutions
 }
 
-// Gets the questions via the channel q from parse.go and 2 channels from main, answers to send the answers given by the user to eval.go
-// and questionCount to send the amount of questions asked to main.go.
-func Asker(questions []string) (answersGiven []string, questionsAsked int) {
 
-	ticker := time.NewTicker(time.Second * 5)
-	var ch chan string
+func Asker(questions, solutions []string, ctx xontext.Context) (points, amount int){
 
-	// for i := 0; i < len(questions); i++ {
+	for i := 0; i < len(questions); i++ {
+		go func(ch chan string, i int) {
+			fmt.Println(questions[i])
+			amount += 1
 
-	go func(ch chan string) {
-		reader := bufio.NewReader(os.Stdin)
-		for i := 0; i < len(questions); i++ {
+			reader := bufio.NewReader(os.Stdin)
+			for {
+				s, err := reader.ReadString('\n')
+				if err != nil {
+					close(ch)
+					return
+				}
+				ch <- s
+			}
+		}(ch, i)
 
-			fmt.Println("What is ", questions[i])
-			questionsAsked += 1
-
-			s, err := reader.ReadString('\n')
-			if err != nil {
-				log.Fatal(err)
-				close(ch)
+		for {
+			select {
+			case answer, ok := <-ch:
+				if !ok {
+					answers <- answersGiven
+					questionCount <- questionsAsked
+					os.Exit()
+				} else {
+					answersGiven = append(answersGiven, strings.Trim(answer, "\n"))
+				}
+			case <-ctx.done:
+				answers <- answersGiven
+				questionCount <- questionsAsked
 				return
 			}
-			ch <- s
-		}
-	}(ch)
-
-	for {
-		select {
-
-		case answer, ok := <-ch:
-			fmt.Println(ok)
-			if !ok {
-				return answersGiven, questionsAsked
-			} else {
-				fmt.Println("hier2")
-
-				answersGiven = append(answersGiven, strings.Trim(answer, "\n"))
-			}
-		case <-ticker.C:
-			return answersGiven, questionsAsked
 		}
 	}
-	fmt.Println("hier")
-	return answersGiven, questionsAsked
 }
-
-// }
 
 // Gets the solutions sent from parse.go via channel s and the answers from ask.go sent via channel answers.
 // Compares the answers to the solutions and returns the amount of points the user scored.
@@ -107,13 +93,17 @@ func Eval(answersGiven, solutions []string) (points int) {
 
 func main() {
 
+	var timer time.Duration = 2
+	ctx := context.Background()
+	ctx, _ = context.WithTimeout(ctx, timer*time.Second)
+
 	//Seems to work
 	questions, solutions := ParseCsv("default.csv")
 
-	//
-	answers, questionsAsked := Asker(questions)
-	fmt.Println(answers, questionsAsked)
-
-	points := Eval(answers, solutions)
-	fmt.Println(points)
+	go func(){
+		if <- 
+	}
+ 
+	points, amount := Asker(questions, solutions, ctx)
+	fmt.Println(points, amount)
 }
