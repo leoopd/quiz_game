0e62358c700b944cc92961718eb398bba7857380
leo <leo.opdenplatz@gmail.com>
2022-09-14 14:01:10 +0200

    implemented the automatic termination after a certain time

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -44,26 +45,26 @@ func Asker(ctx context.Context, ticker *time.Ticker, questions, solutions []stri
 	ch := make(chan int)
 	amount = 1
 
-	for i := 0; i < len(questions); i++ {
-		go func(ctx context.Context, ch chan int, i int) {
+	// for i := 0; i < len(questions); i++ {
+	go func(ctx context.Context, ch chan int) {
 
-			var tmp int
-			reader := bufio.NewReader(os.Stdin)
-			for {
-				fmt.Println("What is: ", questions[i])
-				s, err := reader.ReadString('\n')
-				if err != nil {
-					close(ch)
-					return
-				} else if s == solutions[i] {
-					tmp = 1
-				} else {
-					tmp = 0
-				}
-				ch <- tmp
+		var tmp int
+		reader := bufio.NewReader(os.Stdin)
+		for i := 0; i < len(questions); i++ {
+			fmt.Println("What is: ", questions[i])
+			s, err := reader.ReadString('\n')
+			if err != nil {
+				close(ch)
+				return
+			} else if strings.Trim(s, "\n") == solutions[i] {
+				tmp = 1
+			} else {
+				tmp = 0
 			}
-		}(ctx, ch, i)
-	}
+			ch <- tmp
+		}
+	}(ctx, ch)
+	// }
 
 stdinloop:
 	for {
@@ -97,5 +98,5 @@ func main() {
 	questions, solutions := ParseCsv("default.csv")
 
 	points, amount := Asker(ctx, ticker, questions, solutions)
-	fmt.Println(points, amount)
+	fmt.Printf("\nYou scored %d out of %d possible points\n", points, amount)
 }
