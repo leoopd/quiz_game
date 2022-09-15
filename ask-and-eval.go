d6195b510ab467c90c182cc727194968d2883846
leo <leo.opdenplatz@gmail.com>
2022-09-15 12:36:25 +0200

    added a flag to randomize the order of the questions asked

--- ask-and-eval.go
+++ ask-and-eval.go
@@ -4,36 +4,51 @@ import (
 	"bufio"
 	"context"
 	"fmt"
+	"math/rand"
 	"os"
 	"strings"
 	"time"
 )
 
-func asker(ctx context.Context, ticker *time.Ticker, questions, solutions []string) (points, amount int) {
+func asker(ctx context.Context, ticker *time.Ticker, questions, solutions []string, shuffleQuestions bool) (points, amount int) {
 
 	ctx, cancel := context.WithCancel(ctx)
 
 	ch := make(chan int)
 	amount = 1
 
-	go func(ctx context.Context, ch chan int) {
+	//Mechanism to randomize the order of the questions asked if the user wants to.
+	var order []int
+	if shuffleQuestions {
+		for i := range questions {
+			order = append(order, i)
+		}
+		rand.Seed(time.Now().UnixNano())
+		rand.Shuffle(len(order), func(i, j int) { order[i], order[j] = order[j], order[i] })
+	} else {
+		for i := range questions {
+			order = append(order, i)
+		}
+	}
+
+	go func(ctx context.Context, ch chan int, order []int) {
 
 		var tmp int
 		reader := bufio.NewReader(os.Stdin)
 		for i := 0; i < len(questions); i++ {
-			fmt.Printf("What is: %s?\n", questions[i])
+			fmt.Printf("What is: %s?\n", questions[order[i]])
 			s, err := reader.ReadString('\n')
 			if err != nil {
 				close(ch)
 				return
-			} else if strings.Trim(s, "\n") == solutions[i] {
+			} else if strings.Trim(s, "\n") == solutions[order[i]] {
 				tmp = 1
 			} else {
 				tmp = 0
 			}
 			ch <- tmp
 		}
-	}(ctx, ch)
+	}(ctx, ch, order)
 
 stdinloop:
 	for {
--- main.go
+++ main.go
@@ -9,11 +9,14 @@ import (
 
 func main() {
 
+	//Flags, one for the length of the quiz, one for the link to a quiy and one to randomiye the questions.
 	definedTime := flag.Int("time", 1, "specifies the time limit for the quiz. Format: 1s/1m/1h")
 	quizLink := flag.String("quiz", "default.csv", "specifies the link to the quiz. Format: dir/name.csv")
+	shuffleQuestions := flag.Bool("random", false, "randomizes the order of questions")
 	flag.Parse()
 	questions, solutions := parseCsv(*quizLink)
 
+	//Staring sequence, asks the user to press a specified button and counts down from 3 after.
 	fmt.Println("Please press 'enter' to start the quiz")
 	fmt.Scanln()
 	fmt.Println("The quiz starts in 3...")
@@ -25,10 +28,12 @@ func main() {
 	fmt.Println("GO!")
 	time.Sleep(333 * time.Millisecond)
 
+	//Mechanism to terminate the quiz right when the specified timelimit is reached
 	ctx := context.Background()
 	ticker := time.NewTicker(time.Duration(*definedTime) * time.Second)
 
-	points, amount := asker(ctx, ticker, questions, solutions)
+	points, amount := asker(ctx, ticker, questions, solutions, *shuffleQuestions)
 
+	//Final output that shows the amount of points the user scored and how many questions got asked
 	fmt.Printf("\nYou scored %d out of %d possible points\n", points, amount)
 }
